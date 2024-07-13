5737a3dd8a37f3facb194ad65ff6cb3338f5c662
Deni Tutaev <tutaevdeni818@gmail.com>
2024-07-13 17:20:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,20 +9,24 @@ bot = commands.Bot(intents=discord.Intents.all(), command_prefix='/')
 async def command_info(ctx: commands.Context):
     await ctx.send('Я демонстрационный бот!')
 
+
 @bot.command('weather')
 async def command_weather(ctx: commands.Context):
     await ctx.send('Погода сегодня класс.')
+
 
 @bot.command('greet')
 async def command_greet(ctx: commands.Context):
     user_name = ctx.message.author.name
     await ctx.send(f'Привет, {user_name}!')
 
+
 @bot.command('time')
 async def command_time(ctx: commands.Context):
     now = datetime.now()
     current_time = now.strftime("%Y-%m-%d %H:%M:%S")
     await ctx.send(f'Текущее время: {current_time}')
+
 
 @bot.command('calculate')
 async def command_calculate(ctx: commands.Context, *, expression: str):
@@ -32,6 +36,7 @@ async def command_calculate(ctx: commands.Context, *, expression: str):
     except Exception as e:
         await ctx.send(f'Ошибка: {str(e)}')
 
+
 @bot.command('picture')
 async def command_picture(ctx: commands.Context):
     images = ['picture1.jpg', 'picture2.jpg', 'picture3.jpg']
@@ -40,5 +45,11 @@ async def command_picture(ctx: commands.Context):
     with open('images/' + image_name, 'rb') as file:
         image = discord.File(file)
     await ctx.send('Лови Картинку!', file=image)
+
+
+
+
+
+
 
 bot.run('token')
