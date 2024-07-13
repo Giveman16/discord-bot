df848d6d7728dd5d97f74ca2e0977a21e54618c8
Deni Tutaev <tutaevdeni818@gmail.com>
2024-07-13 17:19:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
 import discord
 from discord.ext import commands
 from datetime import datetime
+import random
 
-bot = commands.Bot(intents=discord.Intents.all(), command_prefix='/') 
+bot = commands.Bot(intents=discord.Intents.all(), command_prefix='/')
 
 @bot.command('info')
-async def command_info(ctx:commands.Context):
+async def command_info(ctx: commands.Context):
     await ctx.send('Я демонстрационный бот!')
 
 @bot.command('weather')
-async def command_weather(ctx:commands.Context):
+async def command_weather(ctx: commands.Context):
     await ctx.send('Погода сегодня класс.')
-
 
 @bot.command('greet')
 async def command_greet(ctx: commands.Context):
     user_name = ctx.message.author.name
     await ctx.send(f'Привет, {user_name}!')
 
-
 @bot.command('time')
 async def command_time(ctx: commands.Context):
     now = datetime.now()
     current_time = now.strftime("%Y-%m-%d %H:%M:%S")
     await ctx.send(f'Текущее время: {current_time}')
-
 
 @bot.command('calculate')
 async def command_calculate(ctx: commands.Context, *, expression: str):
@@ -33,5 +31,14 @@ async def command_calculate(ctx: commands.Context, *, expression: str):
         await ctx.send(f'Результат: {result}')
     except Exception as e:
         await ctx.send(f'Ошибка: {str(e)}')
+
+@bot.command('picture')
+async def command_picture(ctx: commands.Context):
+    images = ['picture1.jpg', 'picture2.jpg', 'picture3.jpg']
+    weights = [0.6, 0.3, 0.1] 
+    image_name = random.choices(images, weights=weights, k=1)[0]
+    with open('images/' + image_name, 'rb') as file:
+        image = discord.File(file)
+    await ctx.send('Лови Картинку!', file=image)
 
 bot.run('token')
